670d5e98f9966fa24cf6a873bd563cd5e912319c
Andrey Kovalchuk <mr.kovalchuk.andrey@gmail.com>
2019-03-24 17:06:51 +0300

    create creeps

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const roleUpgrader = require('role.upgrader');
 const roleBuilder = require('role.builder');
 
 module.exports.loop = function () {
+    for(const i in Game.spawns) {
+        Game.spawns[i].createCreep(['WORK', 'MOVE', 'MOVE', 'CARRY']);
+    }
 
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
