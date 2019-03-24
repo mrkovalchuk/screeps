928720dfe0358fc5d966fab2f7b28da8fc4ac72a
Andrey Kovalchuk <mr.kovalchuk.andrey@gmail.com>
2019-03-24 20:55:22 +0300

    big changes

--- main.js
+++ main.js
@@ -3,12 +3,28 @@ const roleUpgrader = require('role.upgrader');
 const roleBuilder = require('role.builder');
 
 module.exports.loop = function () {
-    for(let i in Game.spawns) {
-        Game.spawns[i].spawnCreep([WORK, MOVE, MOVE, CARRY], {memory: {role: 'harvest'}});
-    }
+    var upgraders = _.filter(Game.creeps, function(creep){ return creep.memory.role === 'upgrader'})
+    var builders = _.filter(Game.creeps, function(creep){ return creep.memory.role === 'builder'})
 
-    for(let name in Game.creeps) {
-        let creep = Game.creeps[name];
+    for(const i in Game.spawns) {
+        const creepName = 'Ball#'+ Math.floor(Math.random() * 1000)
+
+        if(builders.length < Game.creeps.length/2) {
+            Game.spawns[i].spawnCreep([WORK, WORK, MOVE, CARRY], creepName, {memory: {role: 'builder'}});
+        }
+
+        else if(upgraders.length < 2) {
+            Game.spawns[i].spawnCreep([WORK, MOVE, MOVE, CARRY], creepName, {memory: {role: 'upgrader'}});
+        }
+
+        else{
+            if(Game.spawns[i].energyCapacity === 300){
+                Game.spawns[i].spawnCreep([WORK, MOVE, MOVE, CARRY, CARRY], creepName, {memory: {role: 'harvester'}});
+            }
+        }
+    }
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name];
         if(creep.memory.role === 'harvester') {
             roleHarvester.run(creep);
         }
--- role.harvester.js
+++ role.harvester.js
@@ -2,13 +2,22 @@ var roleHarvester = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
+        let workers_by_node = {}
         if(creep.carry.energy < creep.carryCapacity) {
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+            var source = creep.room.find(FIND_SOURCES)[0];
+            creep.memory.source = source
+            console.log(workers_by_node)
+            if(workers_by_node[source].length > 5) {
+                var source = creep.room.find(FIND_SOURCES)[1];
+                creep.memory.source = source
+            }
+            workers_by_node[source] += 1
+            if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
             }
         }
         else {
+            creep.moveTo()
             var targets = creep.room.find(FIND_STRUCTURES, {
                 filter: (structure) => {
                     return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
