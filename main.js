f7c0423a8850bd0cca0482a323dcac40c91f96c6
Andrey Kovalchuk <mr.kovalchuk.andrey@gmail.com>
2019-03-24 17:27:16 +0300

    fix creeps factory

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ const roleUpgrader = require('upgrader');
 const roleBuilder = require('builder');
 
 module.exports.loop = function () {
-    for(const i in Game.spawns) {
-        Game.spawns[i].spawnCreep([WORK, MOVE, MOVE, CARRY], {'role': 'harvest'});
+    for(let i in Game.spawns) {
+        Game.spawns[i].spawnCreep([WORK, MOVE, MOVE, CARRY], {memory: {role: 'harvest'}});
     }
 
-    for(var name in Game.creeps) {
-        var creep = Game.creeps[name];
+    for(let name in Game.creeps) {
+        let creep = Game.creeps[name];
         if(creep.memory.role === 'harvester') {
             roleHarvester.run(creep);
         }
