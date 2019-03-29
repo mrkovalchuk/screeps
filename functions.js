932c42d707092f844435f57309945dfbeaf6d317
Andrey Kovalchuk <mr.kovalchuk.andrey@gmail.com>
2019-03-29 21:26:38 +0300

    change harvester check working

--- functions.js
+++ functions.js
@@ -90,7 +90,7 @@ const transfer_energy_only_to_container = function (creep) {
 
 
 const harvest_energy = function (creep) {
-    if(creep.carry.energy < creep.carryCapacity && !creep.memory.working) {
+    if(creep.carry.energy < creep.carryCapacity) {
         const source = creep.pos.findClosestByPath(creep.room.find(FIND_SOURCES));
 
         if(creep.harvest(source) === ERR_NOT_IN_RANGE) {
--- role.harvester.js
+++ role.harvester.js
@@ -9,13 +9,6 @@ const roleHarvester = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-        if (creep.memory.working && creep.carry.energy === 0) {
-            creep.memory.working = false;
-        }
-        else if (!creep.memory.building && creep.carry.energy > 0) {
-            creep.memory.working = true;
-        }
-
         for(let i in functions){
             if(functions[i](creep) === true){
                 break;
