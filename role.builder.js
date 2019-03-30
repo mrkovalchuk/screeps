aa9c0db0bb2fccad31260dfc5b927f5a5a1c00e6
Andrey Kovalchuk <mr.kovalchuk.andrey@gmail.com>
2019-03-30 17:41:35 +0300

    Revert "access to harvest for builder"
    This reverts commit 1d9b029c

--- role.builder.js
+++ role.builder.js
@@ -1,10 +1,9 @@
-const harvest_energy = require("./functions").harvest_energy;
 const upgrade_controller = require("./functions").upgrade_controller;
 const use_withdraw = require("./functions").use_withdraw;
 const repair_structure = require("./functions").repair_structure;
 const build_structure = require("./functions").build_structure;
 
-const functions = [build_structure, repair_structure, upgrade_controller, use_withdraw, harvest_energy];
+const functions = [build_structure, repair_structure, upgrade_controller, use_withdraw];
 const roleBuilder = {
 
     /** @param {Creep} creep **/
