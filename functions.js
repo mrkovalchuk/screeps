44e387d618f41a04eca5a7d11f6a46af56ba117f
Andrey Kovalchuk <mr.kovalchuk.andrey@gmail.com>
2019-03-31 20:58:08 +0300

    sort spot by flag

--- functions.js
+++ functions.js
@@ -91,14 +91,16 @@ const transfer_energy_only_to_container = function (creep) {
 
 const external_harvest = function (creep) {
     if(creep.carry.energy < creep.carryCapacity) {
-        const source = creep.pos.findClosestByPath(creep.room.find(FIND_SOURCES));
+        const source = creep.pos.findClosestByPath(creep.room.find(FIND_SOURCES, {
+            filter: (structure) => {
+                return structure.pos === Game.flags.EnergySpot1.pos
+            } }));
 
-        if(source.pos === Game.flags.EnergySpot1.pos){
-            if(creep.harvest(source) === ERR_NOT_IN_RANGE) {
-            creep.moveTo(source);
-            }
-            return true
+        if(creep.harvest(source) === ERR_NOT_IN_RANGE) {
+        creep.moveTo(source);
         }
+        return true
+
 
     }
     return false
