ef5e934ef6765b5ef1d021ba9b2e490809193e03
Andrey Kovalchuk <mr.kovalchuk.andrey@gmail.com>
2019-09-20 22:45:29 +0300

    change settings

--- settings.js
+++ settings.js
@@ -2,20 +2,20 @@ const ROOM_CREEP_SET = {
     main: {
         harvesters_mini: 4,
         harvesters: 6,
-        builders: 0,
+        builders: 4,
         transporters: {
-            from: 0, in: 4
+            from: 0, in: 2
         },
-        upgraders: 0,
-        attack_milli: 4,
-        attack_rangers: 7,
+        upgraders: 1,
+        attack_milli: 0,
+        attack_rangers: 0,
     },
     energy_room: {explorer_builders: 0, explorer_harvester: 0,  explorer_transporters: 0, transporters: {from: 0, in: 0}}
 };
 
 const ROOM_CREEPS = {
     E45N18: ROOM_CREEP_SET.energy_room,
-    E44N18: ROOM_CREEP_SET.main
+    W44N49: ROOM_CREEP_SET.main
 };
 
 module.exports.ROOM_CREEPS = ROOM_CREEPS;
